69c6aee139565676daf489398765b268f83e82cd
Steve Butler <stephbu@gmail.com>
2019-12-02 22:55:51 -0800

    Day 3 test variable naming cleanup

--- day3/day3_test.go
+++ day3/day3_test.go
@@ -10,10 +10,10 @@ import (
 
 func TestPart1Sample1(t *testing.T) {
 
-	traceA := "R8,U5,L5,D3"
-	traceB := "U7,R6,D4,L4"
+	steps1 := "R8,U5,L5,D3"
+	steps2 := "U7,R6,D4,L4"
 
-	dist, err := getDistanceFromSteps(true, traceA, traceB)
+	dist, err := getDistanceFromSteps(true, steps1, steps2)
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,10 +22,10 @@ func TestPart1Sample1(t *testing.T) {
 
 func TestPart1Sample2(t *testing.T) {
 
-	traceA := "R75,D30,R83,U83,L12,D49,R71,U7,L72"
-	traceB := "U62,R66,U55,R34,D71,R55,D58,R83"
+	steps1 := "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+	steps2 := "U62,R66,U55,R34,D71,R55,D58,R83"
 
-	dist, err := getDistanceFromSteps(true, traceA, traceB)
+	dist, err := getDistanceFromSteps(true, steps1, steps2)
 	if err != nil {
 		t.Error(err)
 	}
@@ -34,10 +34,10 @@ func TestPart1Sample2(t *testing.T) {
 
 func TestPart1Sample3(t *testing.T) {
 
-	traceA := "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-	traceB := "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+	steps1 := "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+	steps2 := "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
 
-	dist, err := getDistanceFromSteps(true, traceA, traceB)
+	dist, err := getDistanceFromSteps(true, steps1, steps2)
 	if err != nil {
 		t.Error(err)
 	}
@@ -56,10 +56,10 @@ func TestPart1PuzzleInput(t *testing.T) {
 
 func TestPart2Sample1(t *testing.T) {
 
-	traceA := "R8,U5,L5,D3"
-	traceB := "U7,R6,D4,L4"
+	steps1 := "R8,U5,L5,D3"
+	steps2 := "U7,R6,D4,L4"
 
-	dist, err := getDistanceFromSteps(false, traceA, traceB)
+	dist, err := getDistanceFromSteps(false, steps1, steps2)
 	if err != nil {
 		t.Error(err)
 	}
@@ -68,10 +68,10 @@ func TestPart2Sample1(t *testing.T) {
 
 func TestPart2Sample2(t *testing.T) {
 
-	traceA := "R75,D30,R83,U83,L12,D49,R71,U7,L72"
-	traceB := "U62,R66,U55,R34,D71,R55,D58,R83"
+	steps1 := "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+	steps2 := "U62,R66,U55,R34,D71,R55,D58,R83"
 
-	dist, err := getDistanceFromSteps(false, traceA, traceB)
+	dist, err := getDistanceFromSteps(false, steps1, steps2)
 	if err != nil {
 		t.Error(err)
 	}
@@ -80,10 +80,10 @@ func TestPart2Sample2(t *testing.T) {
 
 func TestPart2Sample3(t *testing.T) {
 
-	traceA := "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-	traceB := "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+	steps1 := "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+	steps2 := "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
 
-	dist, err := getDistanceFromSteps(false, traceA, traceB)
+	dist, err := getDistanceFromSteps(false, steps1, steps2)
 	if err != nil {
 		t.Error(err)
 	}
